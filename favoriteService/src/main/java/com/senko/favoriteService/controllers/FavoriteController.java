d8f5e7997ae19019d42f87da2867012bb5b1e77e
selcuk cabuk <cabuk.selcuk@gmail.com>
2023-09-16 12:03:01 +0200

    improve

---
+++ favoriteService/src/main/java/com/senko/favoriteService/controllers/FavoriteController.java
@@ -0,0 +1,12 @@
+package com.senko.favoriteService.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/favorite")
+public class FavoriteController {
+
+    
+
+}
---
+++ favoriteService/src/test/java/com/senko/favoriteService/FavoriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senko.favoriteService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
